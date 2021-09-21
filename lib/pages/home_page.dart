6f73206da6019d050a8e2dcd4f447da3bc945c12
Sankaranarayanan Sengunther <shankar.muthusamy4@gmail.com>
2021-09-21 16:48:52 +0530

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,134 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, avoid_print, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:radio/model/radio.dart';
+import 'package:radio/utils/ai_util.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<MyRadio> radios;
+  @override
+  void initState() {
+    super.initState();
+    fetchRadios();
+  }
+
+  fetchRadios() async {
+    final radioJson = await rootBundle.loadString("assets/radio.json");
+    radios = MyRadioList.fromJson(radioJson).radios;
+
+    print(radios);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(),
+      body: Stack(
+        children: [
+          VxAnimatedBox()
+              .size(context.screenWidth, context.screenHeight)
+              .withGradient(
+                LinearGradient(
+                  colors: [
+                    AIColors.primaryColor1,
+                    AIColors.primaryColor2,
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              )
+              .make(),
+          AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            centerTitle: true,
+            title: 'AI Radio'.text.xl4.bold.white.make().shimmer(
+                  primaryColor: Vx.purple300,
+                  secondaryColor: Colors.white,
+                ),
+          ).h(100.0).p16(),
+          VxSwiper.builder(
+            itemCount: radios.length,
+            aspectRatio: 1.0,
+            itemBuilder: (context, index) {
+              final rad = radios[index];
+
+              return VxBox(
+                child: ZStack(
+                  [
+                    Positioned(
+                      top: 0.0,
+                      right: 0.0,
+                      child: VxBox(
+                        child: rad.category.text.uppercase.white.make().px16(),
+                      )
+                          .height(40)
+                          .black
+                          .alignCenter
+                          .withRounded(value: 10.0)
+                          .make(),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: VStack(
+                        [
+                          rad.name.text.xl3.white.bold.make(),
+                          5.heightBox,
+                          rad.tagline.text.sm.white.semiBold.make(),
+                        ],
+                        crossAlignment: CrossAxisAlignment.center,
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.center,
+                      child: [
+                        Icon(
+                          CupertinoIcons.play_circle,
+                          color: Colors.white,
+                        ),
+                        10.heightBox,
+                        "Double Tap to Play".text.gray300.make(),
+                      ].vStack(),
+                    ),
+                  ],
+                ),
+              )
+                  .clip(Clip.antiAlias)
+                  .bgImage(
+                    DecorationImage(
+                      image: NetworkImage(rad.image),
+                      fit: BoxFit.cover,
+                      colorFilter: ColorFilter.mode(
+                          Colors.black.withOpacity(0.3), BlendMode.darken),
+                    ),
+                  )
+                  .border(color: Colors.black, width: 5.0)
+                  .withRounded(value: 60.0)
+                  .make()
+                  .onInkDoubleTap(() {})
+                  .p16();
+            },
+          ).centered(),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Icon(
+              CupertinoIcons.stop_circle,
+              color: Colors.white,
+              size: 50.0,
+            ),
+          ).pOnly(bottom: context.percentHeight * 12),
+        ],
+        fit: StackFit.expand,
+      ),
+    );
+  }
+}
